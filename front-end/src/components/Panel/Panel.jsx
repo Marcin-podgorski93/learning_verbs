703880ed47c8b6fdd5ae91f32ad9aad1d704ca63
Marcin <56306146+Marcin-podgorski93@users.noreply.github.com>
2025-03-28 14:35:08 +0000

    added FilterButton also integrate them to can filter by category

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { List } from "../List/List";
 import { Form } from "../Form/Form";
+import { FilterButton } from "../FilterButton/FilterButton";
 import styles from "./Panel.module.css";
 
 export function Panel() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3000/words")
@@ -38,6 +40,15 @@ export function Panel() {
     });
   }
 
+  function handleFIlterClick(category) {
+    const params = category ? `?category=${category}` : "";
+    fetch(`http://localhost:3000/words${params}`)
+      .then((res) => res.json())
+      .then((res) => setData(res));
+    setSelectedCategory(category);
+    // console.log(category);
+  }
+
   if (isLoading) {
     return <p>Ładowanie</p>;
   }
@@ -46,6 +57,26 @@ export function Panel() {
     <>
       <section className={styles.section}>
         <Form onFormSubmit={handleFormSubmit} />
+        <div className={styles.filters}>
+          <FilterButton
+            active={selectedCategory === null}
+            onClick={() => handleFIlterClick(null)}
+          >
+            Wszystkie
+          </FilterButton>
+          <FilterButton
+            active={selectedCategory === "noun"}
+            onClick={() => handleFIlterClick("noun")}
+          >
+            Rzeczowniki
+          </FilterButton>
+          <FilterButton
+            active={selectedCategory === "verb"}
+            onClick={() => handleFIlterClick("verb")}
+          >
+            Czasowniki
+          </FilterButton>
+        </div>
         <List data={data} onDeleteItem={handleDeleteItem} />
       </section>
     </>
